e554822bec4bf97a735b428b7f97cb1bdef4babe
yabu38 <132050455+yabu38@users.noreply.github.com>
2023-07-08 07:28:49 +0000

    Update genres/new_index.html.erb to genres/index.html.erb, Add action create,index,edit,update

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,21 @@
 class Admin::GenresController < ApplicationController
+  def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to admin_genres_path
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+    def genre_params
+      params.require(:genre).permit(:name)
+    end
 end
