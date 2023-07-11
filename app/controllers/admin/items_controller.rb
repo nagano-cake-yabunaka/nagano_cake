314c186f39f45b541a8337cbbdd2f19aeadde6fb
yabu38 <132050455+yabu38@users.noreply.github.com>
2023-07-11 15:06:43 +0000

    Finish admin/items/new.html.erb

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -4,6 +4,9 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to admin_item_path(@item.id)
   end
 
   def index
@@ -17,4 +20,9 @@ class Admin::ItemsController < ApplicationController
 
   def update
   end
+
+  private
+    def item_params
+      params.require(:item).permit(:genre_id, :name, :image, :introduction, :price, :is_active)
+    end
 end
