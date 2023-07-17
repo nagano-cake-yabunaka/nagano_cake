a1cebd392cab840cff5679ae5387d91f56dd80d5
yabu38 <132050455+yabu38@users.noreply.github.com>
2023-07-17 14:18:13 +0000

    Update header, public/items/show

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,12 @@
 class Public::ItemsController < ApplicationController
   def index
     @items = Item.page(params[:page]).per(8)
+    unless params[:item_name].nil?
+      Item.find(params[:item_name])
+      redirect_to item_path(params[:id])
+    else
+      redirect_to items_path
+    end
   end
 
   def show
