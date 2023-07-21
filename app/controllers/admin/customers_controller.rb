c6581af6d8007a7b2e08bd0c8d46138b7b721d85
yabu38 <132050455+yabu38@users.noreply.github.com>
2023-07-21 15:19:14 +0000

    Finish admin/customers/edit

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -20,4 +20,5 @@ class Admin::CustomersController < ApplicationController
   private
     def customer_params
       params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :is_deleted, :email)
+    end
 end
