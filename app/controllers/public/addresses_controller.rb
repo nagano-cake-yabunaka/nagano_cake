adef3bb19df4b9c0918e248706eafa9e38488fbd
yabu38 <132050455+yabu38@users.noreply.github.com>
2023-07-24 23:56:41 +0000

    Finish public/addresses/index

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,6 +1,7 @@
 class Public::AddressesController < ApplicationController
   def create
     @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
     @address.save
     redirect_to addresses_path
   end
@@ -24,6 +25,6 @@ class Public::AddressesController < ApplicationController
 
   private
     def address_params
-      params.require(:address).permit(:name, :postal_code, :address)
+      params.require(:address).permit(:name, :postal_code, :address, :customer_id)
     end
 end
