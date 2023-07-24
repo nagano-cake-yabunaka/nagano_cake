ba9609d160754bbfa73a630a0613ee7e0d8a2230
yabu38 <132050455+yabu38@users.noreply.github.com>
2023-07-24 16:23:49 +0000

    Update public/addresses/index

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,8 +1,13 @@
 class Public::AddressesController < ApplicationController
   def create
+    @address = Address.new(address_params)
+    @address.save
+    redirect_to addresses_path
   end
 
   def index
+    @address = Address.new
+    @addresses = Address.all
   end
 
   def edit
@@ -12,5 +17,13 @@ class Public::AddressesController < ApplicationController
   end
 
   def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to addresses_path
   end
+
+  private
+    def address_params
+      params.require(:address).permit(:name, :postal_code, :address)
+    end
 end
