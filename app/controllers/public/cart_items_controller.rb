748e971f5b51f2afbb0049f6f99a5c83d53aa8b8
yabu38 <132050455+yabu38@users.noreply.github.com>
2023-07-27 14:46:14 +0000

    Update public/items/show,public/cart_items/index. But not clear error

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,11 +1,39 @@
 class Public::CartItemsController < ApplicationController
+  def index
+    @cart_items = current_customer.cart_items.all
+  end
+
   def create
-    @item = Item.find(cart_item_params[:item_id])
-    @cart_item.
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.save
+      cart_items = current_customer.cart_items.all
+      redirect_to cart_items_path
+    elsif @cart_item.save
+    　@cart_items = current_customer.cart_items.all
+    　render 'index'
+    else
+      @cart_items = current_customer.cart_items.all
+      render 'index'
+    end
+  end
+
+  def update
+    # cart_item = CartItem.find(cart_item.item.id)
+    # cart_item.update
+    # redirect_to cart_items_path
+  end
+
+  def destroy
+  end
+
+  def destroy_all
   end
 
   private
     def cart_item_params
-      params.require(:cart_item).permit(:item_id, :amount)
+      params.require(:cart_item).permit(:item_id, :amount, :customer_id)
     end
 end
