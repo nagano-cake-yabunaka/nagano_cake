c425e8c4dfd1932f9f412bcff5f6ae94a27ffd32
yabu38 <132050455+yabu38@users.noreply.github.com>
2023-07-28 16:07:51 +0000

    Update public/cart_items/index(clear error)

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,25 +1,26 @@
 class Public::CartItemsController < ApplicationController
   def index
-    @cart_items = current_customer.cart_items.all
+    @cart_items = CartItem.all
   end
 
   def create
-    @cart_item = current_customer.cart_items.new(cart_item_params)
-    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
-      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    @cart_item.item_id = cart_item_params[:item_id]
+    if CartItem.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
       cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item.update(amount: cart_item.amount)
+      redirect_to cart_items_path
+    else
       cart_item.save
       redirect_to cart_items_path
-    elsif @cart_item.save
-    　render 'index'
-    else
-      render 'index'
     end
   end
 
   def update
-    cart_item = CartItem.find(cart_item.item.id)
-    cart_item.update
+    cart_item = CartItem.find(params[:id])
+    cart_item.update(cart_item_params)
     redirect_to cart_items_path
   end
 
@@ -31,6 +32,6 @@ class Public::CartItemsController < ApplicationController
 
   private
     def cart_item_params
-      params.require(:cart_item).permit(:item_id, :amount, :customer_id)
+      params.require(:cart_item).permit(:item_id, :amount)
     end
 end
